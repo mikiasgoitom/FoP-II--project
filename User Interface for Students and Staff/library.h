c41a618410a28ebc2c635d7887d4d1a15de244e1
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2023-08-02 12:01:24 +0300

    Add files via upload

---
+++ User Interface for Students and Staff/library.h
@@ -0,0 +1,21 @@
+//library.h
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+void displayMainMenu();
+void displayStudentMenu();
+void displayStaffMenu();
+void searchBooks();
+void reserveBook();
+void checkAvailability();
+void addBook();
+void removeBook();
+void displayInventory();
+void analyzeDemand();
+void addStaff();
+void removeStaff();
+void displayStaffList();
+void searchStaff();
+
+
+#endif

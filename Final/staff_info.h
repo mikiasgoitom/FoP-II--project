a6b56bada3f15d297a58c7f8074352a909aacc89
Mikias Goitom <mikiasgoitom7@gmail.com>
2023-08-05 07:02:47 +0300

    Add files via upload
    Contains functionalities to store, read and retrieve staff information.

---
+++ Final/staff_info.h
@@ -0,0 +1,221 @@
+/*This functionality will store information about library staff,
+including their contact information, work schedules, and roles.
+This can help in creating shift systems and fulfilling legal requirements for employee registration.
+Again we will be using file handling to store the staff information.*/
+#ifndef staff_info
+#define staff_info
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct birthDate{
+    int day, month, year;
+};
+
+struct librarianInfo{
+    string fullName;
+    birthDate dateOfBirth;
+    string email;
+    string password;
+    string educationLevel;
+    string recidency;
+    string phoneNo;
+    string ID;
+    string role;
+};
+
+struct schedule{
+    string shift;
+};
+
+ostream& operator <<(ostream& print, const librarianInfo& info){
+    print << "Name: "<<info.fullName<<endl;
+    print << "Date of birth: " << info.dateOfBirth.day << "/" << info.dateOfBirth.month << "/" << info.dateOfBirth.year << endl;
+    print << "Email: "<<info.email<<endl;
+    print << "Password: "<<info.password<<endl;
+    print << "Education: "<<info.educationLevel<<endl;
+    print << "Address: "<<info.recidency<<endl;
+    print << "Phone number: "<<info.phoneNo<<endl;
+    print << "Employee ID: "<<info.ID<<endl;
+    print << "Employee role: "<<info.role<<endl<<endl;
+    return print;
+}
+
+void saveEmployeeData(const vector<librarianInfo>& data,const string& fileName){
+    ofstream file(fileName, ios::app);
+    if(file.is_open()){
+        for(const auto& employee: data){
+            file << "Name: "<<employee.fullName<<endl;
+            file << "Date of birth: " << employee.dateOfBirth.day << "/" << employee.dateOfBirth.month << "/" << employee.dateOfBirth.year << endl;
+            file << "Email: "<<employee.email<<endl;
+            file << "Password: "<<employee.password<<endl;
+            file << "Education: "<<employee.educationLevel<<endl;
+            file << "Address: "<<employee.recidency<<endl;
+            file << "Phone number: "<<employee.phoneNo<<endl;
+            file << "Employee ID: "<<employee.ID<<endl;
+            file << "Employee role: "<<employee.role<<endl<<endl;
+        }
+        file.close();
+        cout<<"Employee data is saved to file"<<endl;
+    }
+    else{
+        cout<<"Unable to open file for writing"<<endl;
+    }
+}
+
+void readEmployeeData(const string& fileName) {
+    ifstream file(fileName);
+    if (file.is_open()) {
+        librarianInfo employee;
+        string line;
+        while (getline(file, line)) {
+            if (line.find("Name: ") != string::npos) {
+                employee.fullName = line.substr(6);
+            }
+            else if (line.find("Date of birth: ") != string::npos) {
+                sscanf(line.c_str(), "Date of birth: %d/%d/%d", &employee.dateOfBirth.day, &employee.dateOfBirth.month, &employee.dateOfBirth.year);
+            }
+            else if (line.find("Email: ") != string::npos) {
+                employee.email = line.substr(7);
+            }
+            else if (line.find("Password: ") != string::npos) {
+                employee.password = line.substr(10);
+            }
+            else if (line.find("Education: ") != string::npos) {
+                employee.educationLevel = line.substr(11);
+            }
+            else if (line.find("Address: ") != string::npos) {
+                employee.recidency = line.substr(9);
+            }
+            else if (line.find("Phone number: ") != string::npos) {
+                employee.phoneNo = line.substr(14);
+            }
+            else if (line.find("Employee ID: ") != string::npos) {
+                employee.ID = line.substr(13);
+            }
+            else if (line.find("Employee role: ") != string::npos) {
+                employee.role = line.substr(15);
+                cout << employee;
+            }
+        }
+        file.close();
+    }
+    else {
+        cout << "Unable to open file for reading" << endl;
+    }
+}
+
+void readEmployeeDataForSearch(vector<librarianInfo>& hold, const string& fileName) {
+    ifstream file(fileName);
+    if (file.is_open()) {
+        string line;
+        librarianInfo employee;
+        while (getline(file, line)) {
+             // Move librarianInfo employee inside the loop to create a new object for each line
+
+            if (line.find("Name: ") != string::npos) {
+                employee.fullName = line.substr(6);
+            }
+            else if (line.find("Date of birth: ") != string::npos) {
+                sscanf(line.c_str(), "Date of birth: %d/%d/%d", &employee.dateOfBirth.day, &employee.dateOfBirth.month, &employee.dateOfBirth.year);
+            }
+            else if (line.find("Email: ") != string::npos) {
+                employee.email = line.substr(7);
+            }
+            else if (line.find("Password: ") != string::npos) {
+                employee.password = line.substr(10);
+            }
+            else if (line.find("Education: ") != string::npos) {
+                employee.educationLevel = line.substr(11);
+            }
+            else if (line.find("Address: ") != string::npos) {
+                employee.recidency = line.substr(9);
+            }
+            else if (line.find("Phone number: ") != string::npos) {
+                employee.phoneNo = line.substr(14);
+            }
+            else if (line.find("Employee ID: ") != string::npos) {
+                employee.ID = line.substr(13);
+            }
+            else if (line.find("Employee role: ") != string::npos) {
+                employee.role = line.substr(15);
+                cout << employee;
+            }
+
+            hold.push_back(employee); // Add the employee object to the hold vector
+        }
+
+        file.close();
+    }
+    else {
+        cout << "Unable to open file for reading" << endl;
+    }
+}
+
+void searchName(const vector<librarianInfo>& data, const string& name){
+    bool found = false;
+    for(static auto& employee: data){
+        if(employee.fullName == name){
+            found = true;
+        }
+    }
+    if(found == true){
+            cout<<"That name exist.";
+        }
+    if(!found){
+                cout<<"That name doesn't exist";
+        }
+}
+void searchID(const vector<librarianInfo>& data, const string& ID){
+    bool found = false;
+    for(const auto& employee: data){
+        if(employee.ID == ID){
+            found = true;
+        }
+    }
+        if(found == true){
+            cout<<"That ID exist.";
+        }
+        if(!found){
+                cout<<"That ID doesn't exist";
+        }
+}
+
+void removeEmployeeData(vector<librarianInfo>& data, int index, const string& fileName){
+    if(index >= 0 && index <= data.size()){
+        data.erase(data.begin()+index, data.end());
+        // Update the .txt file with the modified data
+        ofstream file(fileName);
+        if (file.is_open()) {
+            int i = 1;
+            for (const auto& employee : data) {
+                file << "\nName: " << employee.fullName << endl;
+                file << "Date of birth: " << employee.dateOfBirth.day << "/" << employee.dateOfBirth.month << "/" << employee.dateOfBirth.year << endl;
+                file << "Email: " << employee.email << endl;
+                file << "Password: " << employee.password << endl;
+                file << "Education: " << employee.educationLevel << endl;
+                file << "Address: " << employee.recidency << endl;
+                file << "Phone number: " << employee.phoneNo << endl;
+                file << "Employee ID: " << employee.ID << endl;
+                file << "Employee role: " << employee.role << endl;
+
+                i++;
+            }
+            file.close();
+            cout << "\nSuccessfully removed employee data and updated the file!\n"<<i;
+    }
+    }
+    else{
+        cout<<"\nWrong index!\n";
+    }
+}
+
+
+
+#endif // staff_info
+
+
